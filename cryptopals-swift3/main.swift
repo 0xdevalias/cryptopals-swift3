97365cf1510f66804091dc759ad41145112f2a5b
Glenn 'devalias' Grant <glenn@devalias.net>
2016-06-18 05:55:19 +1000

    Update source headers to include MIT licese

--- cryptopals-swift3/main.swift
+++ cryptopals-swift3/main.swift
@@ -3,8 +3,10 @@
 //  cryptopals-swift3
 //
 //  Created by Glenn Grant on 18/06/2016.
-//  Copyright © 2016 Glenn 'devalias' Grant. All rights reserved.
+//  Copyright © 2016 Glenn 'devalias' Grant.
 //
+//  This software may be modified and distributed under the terms
+//  of the MIT license.  See the LICENSE file for details.
 
 import Foundation
 
