ddf64848c978de27ef7683f385ea5b31f47cce60
Glenn 'devalias' Grant <glenn@devalias.net>
2016-06-18 05:05:11 +1000

    Initial Commit

---
+++ cryptopals-swift3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cryptopals-swift3
+//
+//  Created by Glenn Grant on 18/06/2016.
+//  Copyright © 2016 Glenn 'devalias' Grant. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
