9d7a22794bf7e283b4a69007be9d68bcb4c5f926
Glenn 'devalias' Grant <glenn@devalias.net>
2016-06-23 14:47:13 +1000

    Start of set 1

---
+++ cryptopals-swift3-lib/Sources/set1.swift
@@ -0,0 +1,28 @@
+//  cryptopals-swift3-lib.swift
+//
+//  Copyright © 2016 Glenn 'devalias' Grant
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license.  See the LICENSE file for details.
+
+// Ref: https://cryptopals.com/sets/1
+
+// 2 Fixed XOR
+// 3 Single-byte XOR cipher
+// 4 Detect single-character XOR
+// 5 Implement repeating-key XOR
+// 6 Break repeating-key XOR
+// 7 AES in ECB mode
+// 8 Detect AES in ECB mode
+
+struct set1 {
+
+    // Q1: Convert hex to base64: https://cryptopals.com/sets/1/challenges/1
+    //   Bytes: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+    //   Base64: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+    // References
+    //   https://sketchytech.blogspot.com.au/2014/08/swift-bytes-and-strings-part-i.html
+    func q1_convertHexToBase64() -> String {
+      return "TODO"
+    }
+}
